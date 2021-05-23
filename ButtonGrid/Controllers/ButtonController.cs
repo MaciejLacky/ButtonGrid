034de647e3cf8c394f0246e66925effdbdf7151c
Maciej Łącki <maciej.lacky@gmail.com>
2021-05-23 15:21:01 +0200

    add ajax function to refres partial view of buttons

--- ButtonGrid/Controllers/ButtonController.cs
+++ ButtonGrid/Controllers/ButtonController.cs
@@ -1,7 +1,11 @@
 ﻿using ButtonGrid.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -51,6 +55,68 @@ namespace ButtonGrid.Controllers
 
 
         }
+
+        public IActionResult ShowOneButton(int buttonNumber)
+        {
+            buttons.ElementAt(buttonNumber).ButtonState = (buttons.ElementAt(buttonNumber).ButtonState + 1) % 4;
+            //1 render a button and save it to string
+            string  buttonString = RenderRazorViewToString(this, "ShowOneButton", buttons.ElementAt(buttonNumber));
+            //2 Generate a win or los string based on state of buttons array
+            bool DidIWinYet = true;
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if (buttons.ElementAt(i).ButtonState != buttons.ElementAt(0).ButtonState)
+                {
+                    DidIWinYet = false;
+                }
+            }
+            string messageString = "";
+            if (DidIWinYet)
+            {
+                messageString = "<p>Congratulations. You WIN!!!</p>";
+            }
+            else
+            {
+                messageString = "<p>Play again</p>";
+            }
+            //3 Assembly a json string that has two parts button string html and win/loss message
+
+            var package = new { part1 = buttonString, part2 = messageString };
+
+            //4 send the json result
+            return Json(package);
+
+
+
+            //return PartialView(buttons.ElementAt(buttonNumber));
+        }
+
+        public static string RenderRazorViewToString(Controller controller, string viewName, object model = null)
+        {
+            controller.ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                IViewEngine viewEngine =
+                    controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as
+                        ICompositeViewEngine;
+                ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, false);
+
+                ViewContext viewContext = new ViewContext(
+                    controller.ControllerContext,
+                    viewResult.View,
+                    controller.ViewData,
+                    controller.TempData,
+                    sw,
+                    new HtmlHelperOptions()
+                );
+                viewResult.View.RenderAsync(viewContext);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+
+
+
     }
 
     
--- ButtonGrid/wwwroot/js/site.js
+++ ButtonGrid/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+
+    $(document).on("click", ".game-button", function (event) {
+        event.preventDefault();
+        var buttonNumber = $(this).val();
+        doButtonUpdate(buttonNumber);
+
+    });
+
+    function doButtonUpdate(buttonNumber) {
+        $.ajax({
+
+            datatype: "json",
+            method: "POST",
+            url: "/button/ShowOneButton",
+            data: { "buttonNumber": buttonNumber },
+            success: function (data) {
+                console.log(data);
+                //data should be a json object with part1 and part2
+                $("#" + buttonNumber).html(data.part1);
+                $(".messageArea").html(data.part2);
+            }
+
+        });
+    };
+
+});
